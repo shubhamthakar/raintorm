181e903daf08d62f72808e1a5e6c16d35e927b54
sthakar3@illinois.edu <sthakar3@illinois.edu>
2024-09-14 16:46:51 -0500

    added 1st tets

--- tests/test_grep.py
+++ tests/test_grep.py
@@ -1,25 +1,24 @@
-# inputs host_name, port, grep_command
-# outputs host_name, grep_output, lines_matched
-
-
-# {test_name -> (grep_command, {host_name -> (grep_out, lines_matched)})}
-
-
+from socket_client_parallel import run_parallel_socket_calls
+from hosts import hosts_dict
+# run_parallel_socket_calls(hosts_dict, port, grep_command)
+# output_dict[host_name] = grep_output
+port = 12345
 
 def test_your_function():
-    # Define inputs
-    param1 = 10
-    param2 = 5
+    grep_command = ['-r', '\[200 OK\] Request from 127.0.0.1', '/home/chaskar2/distributed-logger/tests/test_logs/test_rare_on_one_machine.txt']
+    output_dict = run_parallel_socket_calls(hosts_dict, port, grep_command)
 
-    # Define expected outputs
-    expected_output1 = 15  # param1 + param2
-    expected_output2 = 5   # param1 - param2
-    expected_output3 = 50  # param1 * param2
-
-    # Call the function with the input parameters
-    output1, output2, output3 = your_function(param1, param2)
-
+    expected_output ={
+    'fa24-cs425-6901.cs.illinois.edu': '[200 OK] Request from 127.0.0.1\n',
+    'fa24-cs425-6902.cs.illinois.edu': '',
+    'fa24-cs425-6903.cs.illinois.edu': '',
+    'fa24-cs425-6904.cs.illinois.edu': '',
+    'fa24-cs425-6905.cs.illinois.edu': '',
+    'fa24-cs425-6906.cs.illinois.edu': '',
+    'fa24-cs425-6907.cs.illinois.edu': '',
+    'fa24-cs425-6908.cs.illinois.edu': '',
+    'fa24-cs425-6909.cs.illinois.edu': '',
+    'fa24-cs425-6910.cs.illinois.edu': ''
+}
     # Use assertions to compare actual and expected values
-    assert output1 == expected_output1, f"Expected {expected_output1}, got {output1}"
-    assert output2 == expected_output2, f"Expected {expected_output2}, got {output2}"
-    assert output3 == expected_output3, f"Expected {expected_output3}, got {output3}"
+    assert output_dict == expected_output, f"Expected {expected_output}, got {output_dict}"
