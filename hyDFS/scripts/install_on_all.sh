7f99dc34deaf0a0eeddcb23da31015b092c039fc
sthakar3 <sthakar3@fa24-cs425-6901.cs.illinois.edu>
2024-11-26 17:04:32 -0600

    testing rainstorm worker

--- hyDFS/scripts/install_on_all.sh
+++ hyDFS/scripts/install_on_all.sh
@@ -12,7 +12,7 @@ for i in $(seq -w 01 10); do
     echo "Successfully installed pip on $HOST"
     
     # Install msgpack using pip
-    ssh "$HOST" "pip3 install msgpack"
+    ssh "$HOST" "pip3 install grpcio"
     
     if [ $? -eq 0 ]; then
       echo "Successfully installed msgpack on $HOST"
